4410e20b32f64539f91da50605b166c7909a80e0
HuangChangLing <2082000367@qq.com>
2021-01-18 21:05:59 +0800

    docs：基础构建

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import './index.less';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+function Elements() {
+  return (
+    <div>
+      测试一波
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <Elements></Elements>,
+  document.getElementById('app')
+)
