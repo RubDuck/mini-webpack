15856b32f5534c5e04ae100b380e7be67f0f15a4
HuangChangLing <2082000367@qq.com>
2021-01-21 21:02:48 +0800

    docs：添加多页面打包

--- src/index/index.js
+++ src/index/index.js
@@ -1,13 +1,14 @@
 import './index.less';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import hg from './hg.jpg'
 
 
 function Elements() {
   return (
     <div>
       测试一波
+      <img  src= {hg} />
     </div>
   )
 }
